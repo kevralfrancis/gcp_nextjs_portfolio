6df70799716554a19a262d204f1845060c6da516
Kevral <kevralfrancis29@gmail.com>
2023-01-10 01:03:00 -0500

    file saver updated update Commit of My portfolio for GCP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react';
-import { saveAs } from 'file-saver';
+import { saveAs } from "file-saver";
 import { Inter } from '@next/font/google'
 
 
