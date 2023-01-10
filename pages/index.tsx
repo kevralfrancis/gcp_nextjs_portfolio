47437390ca0b3cb5b899e542748c08a7eb718230
Kevral <kevralfrancis29@gmail.com>
2023-01-10 00:33:03 -0500

    form completed and working update Commit of My portfolio for GCP

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import { useState } from 'react';
+import { saveAs } from 'file-saver';
 import { Inter } from '@next/font/google'
 
 
@@ -7,6 +9,110 @@ import { Inter } from '@next/font/google'
 
 
 export default function Home() {
+
+
+    const [fullname, setFullname] = useState("");
+    const [email, setEmail] = useState("");
+    const [Phone, setPhone] = useState("");
+    const [message, setMessage] = useState("");
+  
+    //   Form validation
+    const [errors, setErrors] = useState({});
+  
+    //   Setting button text
+    const [buttonText, setButtonText] = useState("Send");
+  
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showFailureMessage, setShowFailureMessage] = useState(false);
+
+
+    const saveFile = () => {
+        saveAs(
+          "https://res.cloudinary.com/miamiwrapz-com/image/upload/v1646400566/kevral_resume_w0kmcw.ai",
+          "KevralFrancisCV.pdf"
+        );
+      };
+
+
+  
+    const handleValidation = () => {
+      let tempErrors = {};
+      let isValid = true;
+  
+      if (fullname.length <= 0) {
+        tempErrors["fullname"] = true;
+        isValid = false;
+      }
+      if (email.length <= 0) {
+        tempErrors["email"] = true;
+        isValid = false;
+      }
+      if (Phone.length <= 0) {
+        tempErrors["Phone"] = true;
+        isValid = false;
+      }
+      if (message.length <= 0) {
+        tempErrors["message"] = true;
+        isValid = false;
+      }
+  
+      setErrors({ ...tempErrors });
+      console.log("errors", errors);
+      return isValid;
+    };
+  
+    //   const [form, setForm] = useState(false);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+  
+      let isValidForm = handleValidation();
+  
+      if (isValidForm) {
+        setButtonText("Sending");
+        const res = await fetch("/api/sendgrid", {
+          body: JSON.stringify({
+            email: email,
+            fullname: fullname,
+            Phone: Phone,
+            message: message,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+          method: "POST",
+        });
+  
+        const { error } = await res.json();
+        if (error) {
+          console.log(error);
+          setShowSuccessMessage(false);
+          setShowFailureMessage(true);
+          setButtonText("Send");
+  
+          // Reset form fields
+          setFullname("");
+          setEmail("");
+          setMessage("");
+          setPhone("");
+          return;
+        }
+        setShowSuccessMessage(true);
+        setShowFailureMessage(false);
+        setButtonText("Send");
+        // Reset form fields
+        setFullname("");
+        setEmail("");
+        setMessage("");
+        setPhone("");
+      }
+      console.log(fullname, email, Phone, message);
+    };
+
+
+
+
+
   return (
     <>
 
@@ -157,8 +263,8 @@ export default function Home() {
                     <p>I am a US-based Software Engineer for both frontend and backend development, focused on crafting
                         clean and user‑friendly experiences. I am passionate about building excellent software that
                         improves the lives of those around me.</p>
-                    <button type="button" className="btn btn-primary btn-lg button_gradient">Download CSV</button>
-                    <button type="button" className="btn btn-secondary btn-lg">About Me</button>
+                    <button onClick={saveFile} type="button" className="btn btn-primary btn-lg button_gradient">Download CSV</button>
+                    <a href="#ABOUT-ME">  <button type="button" className="btn btn-secondary btn-lg">About Me</button> </a>
                 </div>
             </div>
         </div>
@@ -240,7 +346,7 @@ export default function Home() {
                             tried to impart to the right team and organization.
                         </p>
 
-                        <button type="button" className="btn btn-primary btn-lg button_gradient">Download CSV </button>
+                        <button onClick={saveFile} type="button" className="btn btn-primary btn-lg button_gradient">Download CSV </button>
 
                     </div>
 
@@ -986,14 +1092,204 @@ height={500}
 
                 <div className="contact-area">
 
-                    <form action="">
-                        <input type="text" placeholder="Company Name" />
-                        <input type="email" placeholder="Email" />
-                        <input type="tel" placeholder="Phone Number" />
-                        <textarea 
-                            placeholder="Any Additional Information..."></textarea>
+                    {/* <form onSubmit={handleSubmit} >
+                            <input type="text" placeholder="Company Name" 
+                            value={fullname}
+                            onChange={(e) => {
+                            setFullname(e.target.value);
+                            }}
+                            name="fullname"
+                            />
 
-                    </form>
+                        <input type="email" placeholder="Email" 
+                           value={email}
+                           onChange={(e) => {
+                           setEmail(e.target.value);
+                            }}
+                           name="fullname"
+                           />
+                        
+                        <input type="tel" placeholder="Phone Number" 
+                            value={phone}
+                            onChange={(e) => {
+                            setPhone(e.target.value);
+                            }}
+                            name="fullname"
+                            />
+                        
+                        <textarea 
+                            placeholder="Any Additional Information..."
+                            name="message"
+                            value={message}
+                            onChange={(e) => {
+                            setMessage(e.target.value);
+                            }}
+                            
+                            ></textarea>
+
+
+<button
+              type="submit"
+              className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center"
+            >
+              Submit
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="text-cyan-500 ml-2"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+
+
+
+
+                    </form> */}
+
+
+
+
+
+
+
+
+
+<form
+          onSubmit={handleSubmit}
+        //   className="rounded-lg shadow-xl flex flex-col px-8 py-8 bg-white dark:bg-blue-500"
+        >
+          <h1 className="text-2xl font-bold dark:text-gray-50">
+            Send a message
+          </h1>
+
+          {/* <label
+            htmlFor="fullname"
+            className="text-gray-500 font-light mt-8 dark:text-gray-50"
+          >
+            Full name<span className="text-red-500 dark:text-gray-50">*</span>
+          </label> */}
+          <input
+            type="text"
+            value={fullname}
+            placeholder="Enter Your Company Name" 
+            onChange={(e) => {
+              setFullname(e.target.value);
+            }}
+            name="fullname"
+            // className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+          {errors?.fullname && (
+            <p className="text-red-500">Fullname cannot be empty.</p>
+          )}
+
+          {/* <label
+            htmlFor="email"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            E-mail<span className="text-red-500">*</span>
+          </label> */}
+          <input
+            type="email"
+            name="email"
+            placeholder="Enter Your Email here..." 
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            // className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+          {errors?.email && (
+            <p className="text-red-500">Email cannot be empty.</p>
+          )}
+
+          {/* <label
+            htmlFor="Phone"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            Phone<span className="text-red-500">*</span>
+          </label> */}
+          <input
+            type="text"
+            name="Phone"
+            placeholder="Enter Your Phone Number" 
+            value={Phone}
+            onChange={(e) => {
+              setPhone(e.target.value);
+            }}
+            // className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          />
+          {errors?.Phone && (
+            <p className="text-red-500">Phone cannot be empty.</p>
+          )}
+          {/* <label
+            htmlFor="message"
+            className="text-gray-500 font-light mt-4 dark:text-gray-50"
+          >
+            Message<span className="text-red-500">*</span>
+          </label> */}
+          <textarea
+            name="message"
+            placeholder="Any Additional Information..."
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value);
+            }}
+            // className="bg-transparent border-b py-2 pl-4 focus:outline-none focus:rounded-md focus:ring-1 ring-green-500 font-light text-gray-500"
+          ></textarea>
+          {errors?.message && (
+            <p className="text-red-500">Message body cannot be empty.</p>
+          )}
+          <div className="flex flex-row items-center justify-start">
+            <button
+              type="submit"
+              className="px-10 mt-8 py-2 bg-[#130F49] text-gray-50 font-light rounded-md text-lg flex flex-row items-center kf-gradient-bg"
+            >
+              {buttonText}
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                className="text-cyan-500 ml-2"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9.00967 5.12761H11.0097C12.1142 5.12761 13.468 5.89682 14.0335 6.8457L16.5089 11H21.0097C21.562 11 22.0097 11.4477 22.0097 12C22.0097 12.5523 21.562 13 21.0097 13H16.4138L13.9383 17.1543C13.3729 18.1032 12.0191 18.8724 10.9145 18.8724H8.91454L12.4138 13H5.42485L3.99036 15.4529H1.99036L4.00967 12L4.00967 11.967L2.00967 8.54712H4.00967L5.44417 11H12.5089L9.00967 5.12761Z"
+                  fill="currentColor"
+                />
+              </svg>
+            </button>
+          </div>
+          <div className="text-left">
+            {showSuccessMessage && (
+              <p className="text-green-500 font-semibold text-sm my-2">
+                Thankyou! Your Message has been delivered.
+              </p>
+            )}
+            {showFailureMessage && (
+              <p className="text-red-500">
+                Oops! Something went wrong, please try again.
+              </p>
+            )}
+          </div>
+        </form>
+
+
+
+
+
+
+
+
+
+
                 </div>
 
 
